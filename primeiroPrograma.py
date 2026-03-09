4aa9c591dee73080e3ae1354dbe412dfea550f56
Lidice Adelino <lidice62216736@alunos.sc.senac.br>
2026-03-09 20:49:53 -0300

    Meu primeiro commit

---
+++ primeiroPrograma.py
@@ -0,0 +1,7 @@
+aluna = input("Qual o seu nome?")
+print("Olá,", aluna)
+idade = input("Qual sua idade?")
+print("Você tem ", idade)
+altura = input("Sua altura é ", altura)
+print = n\
+n1 = input("Digite um numero")
